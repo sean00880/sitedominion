74d784ce9fdb31e5737f3ff1be70393211174adb
sean00880 <theelitereapersanctum@gmail.com>
2023-03-23 09:06:19 -0400

    33rd commit

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps }) {
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossOrigin="anonymous"
+        crossOrigin="anonymous" strategy="lazyOnload"
       />
       <link href="assets/css/fontawesome-5.14.0.min.css" rel="stylesheet" />
         {/* Bootstrap */}
